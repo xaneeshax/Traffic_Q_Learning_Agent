3b250bc1c0be151b85bb6307a56fd6037659a7ed
xaneeshax <64504643+xaneeshax@users.noreply.github.com>
2022-11-28 18:47:39 -0500

    Added second Q-criteria + documentation

---
+++ run_q.py
@@ -0,0 +1,307 @@
+import traci
+import traci.constants as tc
+import random
+import pandas as pd
+
+# Path to the osm.sumocfg file
+ANEESHA_PATH = "/Users/aneesha/sumo/tools/2022-11-10-00-32-34/osm.sumocfg"
+MAURA_PATH = "C:/Users/Maura/OneDrive - Northeastern University/Artificial Intelligence/Traffic_Q_Learning_Agent/osm.sumocfg"
+
+
+class Sim():
+
+    def __init__(self, epsilon=0.5, iters=10000):
+        # Traffic Light Junction ID
+        self.junction = 'J0'
+        # Full logic definition of light settings at junction
+        self.full_def = traci.trafficlight.getCompleteRedYellowGreenDefinition(self.junction)[
+            0]
+        # Possible TLS for the junction
+        self.states = [phase.state for phase in self.full_def.phases]
+        # Record of Light States (one for each step in simulation)
+        self.actions_taken = []
+        # Wait time of the previous step & lane current step
+        self.prev_wait, self.cur_wait = None, None
+        # Store lane information corresponding to the 8 lane intersection
+        self.lanes = ["426843745#0", "798289594#3", "-8644213#9", "102519704#0", "426843743", "798289594#0", "8644213#8",
+                      "426493699#0"]
+        # The Q-values
+        '''
+        Store Q-values in this form:
+        {lane_x_has_most_cars: {action_1: q_val, action_2: q_val, ..., action_n: q_val}, ...,
+        lane_z_has_most_cars: {{action_1: q_val, action_2: q_val, ..., action_n: q_val}}
+        '''
+        self.qs = {}
+        # Q-value parameters
+        self.alpha, self.gamma, self.epsilon = 0.01, 0.9, epsilon
+        # Step Counter
+        self.step = 0
+        # Number of iterations
+        self.iters = iters
+        # Wait Time Data & Lane - Vehicle Info
+        self.data, self.vehs_data = [], []
+
+    def run_simulation(self):
+        """
+        Runs the simulation for teh specified number of timesteps
+        """
+
+        while self.step < self.iters:
+            # Get current light state
+            # Example light state:  GGGgrGrrrrrrGrrrr
+            cur_state = traci.trafficlight.getRedYellowGreenState(
+                self.junction)
+
+            # Get action to take
+            # Exmaple action state:  GGGgrGrrrrrrGrrrr
+            action = self.getAction(cur_state)
+
+            # Set traffic light to action and move to next time step
+            traci.trafficlight.setRedYellowGreenState(self.junction, action)
+            traci.simulationStep()
+
+            # Get updated wait time; reward is how much shorter the wait is now
+            self.updateWaitTime()
+            reward = self.prev_wait - self.cur_wait
+
+            # Get current longest lane
+            lane = self.findLongestQueueLane()
+
+            # Find new current longest lane
+            afterStepLongestLane = self.findLongestQueueLane()
+
+            # Update Q-values
+            self.update_qs(lane, action, afterStepLongestLane, reward)
+
+            # End simulation if there are no more cars in play
+            if len(traci.vehicle.getIDList()) == 0:
+                break
+
+            self.step += 1
+
+        # Store Simulation Data (for visuals still working on this)
+        # df = pd.DataFrame(self.data)
+        # df.to_csv('wait-times.csv')
+        # df_2 = pd.DataFrame(self.vehs_data)
+        # df_2.to_csv('vehs-data.csv')
+
+    def updateWaitTime(self):
+        """
+        Updates the previous wait time and current wait time that are used to calculate the reward
+        """
+
+        # Set wait times to 0 for the first step
+        if self.step == 0:
+            self.prev_wait = 0
+            self.cur_wait = 0
+        else:
+            step_wait = 0
+            # Iterate through all the lanes and get the cumulative wait time on that lane
+            for lane in self.lanes:
+                indiv_wait_time = traci.edge.getWaitingTime(lane)
+                self.data.append([self.step, lane, indiv_wait_time])
+                # Get total wait time of all the lanes
+                step_wait += indiv_wait_time
+            # Update the previous wait time and the current wait time
+            self.prev_wait = self.cur_wait
+            self.cur_wait = step_wait
+
+    def findLongestQueueLane_orig(self):
+        """
+        Q-Criteria #1
+        Finds the lane with the most number of cars and returns this value. The intuition for the model 
+        is that the lane with the highest number of cars waiting is of the highest priority to ensure 
+        proper traffic flow
+        """
+
+        num_vehs = {}
+        # Get number of vehicles in each lane
+        for lane in self.lanes:
+            num_vehs[lane] = traci.edge.getLastStepVehicleNumber(lane)
+        # Append data for visualization
+        self.vehs_data.append(
+            [f'{self.step}@{key}@{value}' for key, value in num_vehs.items()])
+        # Return the lane with most vehicles waiting
+        # This is the lane that we want to begin clearing out
+        return max(num_vehs, key=num_vehs.get)
+
+    def findHighestWaitTimeLane(self):
+        """
+        Q-Criteria #2
+        Finds the lane with the most number of cars and returns this value. The intuition for the model 
+        is that the lane with the highest number of cars waiting is of the highest priority to ensure 
+        proper traffic flow
+        """
+
+        num_vehs = {}
+        # Get number of vehicles in each lanFe
+        for lane in self.lanes:
+            num_vehs[lane] = traci.edge.getLastStepVehicleIDs(lane)
+            num_vehs[lane] = sum([traci.vehicle.getAccumulatedWaitingTime(
+                veh_id) for veh_id in num_vehs[lane]])
+
+        # Append data for visualization
+        self.vehs_data.append(
+            [f'{self.step}@{key}@{value}' for key, value in num_vehs.items()])
+        # Return the lane with most vehicles waiting
+        # This is the lane that we want to begin clearing out
+        return max(num_vehs, key=num_vehs.get)
+
+    def getQValue(self, max_lane, action):
+        """
+        Params:
+            max_lane (int) : ID of 1 of 8 lanes in the junction
+            action (str) : A legal traffic light state 
+
+        Returns the Q-value for the specified lane and actions
+        """
+
+        if max_lane in self.qs:
+            if action in self.qs[max_lane]:
+                return self.qs[max_lane][action]
+        return 0
+
+    def computeValueFromQValues(self, lane, ryg_state):
+        """
+        Params:
+            lane (int) : ID of 1 of 8 lanes in the junction
+            ryg_state (str) : A legal traffic light state 
+
+        Returns the Q-value of the best traffic light state for the lane with the most number of vehicles
+        waiting
+        """
+
+        # Get the legal traffic light states and the lane with the most number of cars waiting
+        actions = self.getLegalActions(ryg_state)
+        lane = self.findLongestQueueLane()
+        # Q-value for the first action (placeholder for the max Q value)
+        val = self.getQValue(lane, actions[0])
+        # Iterate through the legal actions to find the action with the highest Q value
+        for i in range(1, len(actions)):
+            # If this action produces a higher Q-value, save its value
+            new_val = self.getQValue(lane, actions[i])
+            if new_val > val:
+                val = new_val
+        # Return highest Q-value
+        return val
+
+    def getLegalActions(self, ryg_state):
+        """
+        Params:
+            ryg_state (str) : A legal traffic light state 
+
+        Returns a list of valid traffic light states given the current one. 
+        """
+
+        # Yellow light must be at least 3 steps long to avoid emergency stops
+        # Note: ryg_state is the same as self.actions_taken[-1]
+        if 'y' in ryg_state:
+            # Make sure enough actions have been taken - if not enough have been taken, can't check prev states
+            if len(self.actions_taken) > 2:
+                # If the 2 actions before this one aren't the same as this one, light must then stay the same
+                if self.actions_taken[-2] != ryg_state or self.actions_taken[-3] != ryg_state:
+                    return [ryg_state]
+        # Self (don't change), or a red can become green, a yellow can become red, a green can become yellow
+        if ryg_state == 'GGGgrGrrrrrrGrrrr':
+            return [ryg_state, 'yyyyryrrrrrryrrrr']
+        if ryg_state == 'yyyyryrrrrrryrrrr':
+            return [ryg_state, 'rrrrGrrrrGGrrrrrr', 'rrrrrrrrrGGGrrrrr', 'rrrrrGGggrrrrGGgg']
+        if ryg_state == 'rrrrGrrrrGGrrrrrr':
+            return [ryg_state, 'rrrryrrrryyrrrrrr']
+        if ryg_state == 'rrrryrrrryyrrrrrr':
+            return [ryg_state, 'GGGgrGrrrrrrGrrrr', 'rrrrrGGggrrrrGGgg']
+        if ryg_state == 'rrrrrrrrrGGGrrrrr':
+            return [ryg_state, 'rrrrrrrrryyyrrrrr']
+        if ryg_state == 'rrrrrrrrryyyrrrrr':
+            return [ryg_state, 'GGGgrGrrrrrrGrrrr', 'rrrrrGGggrrrrGGgg']
+        if ryg_state == 'rrrrrGGggrrrrGGgg':
+            return [ryg_state, 'rrrrryyyyrrrryyyy']
+        if ryg_state == 'rrrrryyyyrrrryyyy':
+            return [ryg_state, 'rrrrrrrrrGGGrrrrr', 'rrrrGrrrrGGrrrrrr']
+        raise Exception(
+            'Unknown light state encountered - don\'t know possible next moves')
+
+    def computeActionFromQValues(self, ryg_state):
+        """
+        Params:
+            ryg_state (str) : A legal traffic light state 
+
+        Returns the action that yields the highest Q-value for the lane with the most number of vehicles
+        waiting
+        """
+
+        # Get the legal traffic light states and the lane with the most number of cars waiting
+        actions = self.getLegalActions(ryg_state)
+        action = actions[0]
+        lane = self.findLongestQueueLane()
+        # Q value for best action
+        val = self.getQValue(lane, action)
+        # Iterate through the legal actions to find the action with the highest Q value
+        for i in range(1, len(actions)):
+            new_val = self.getQValue(lane, actions[i])
+            # If this action produces a higher Q-value, save its value
+            if new_val > val:
+                action = actions[i]
+                val = new_val
+        # Return action with the highest Q-value
+        return action
+
+    def getAction(self, ryg_state):
+        """
+        Params:
+            ryg_state (str) : A legal traffic light state 
+
+        Either picks a random state to explore the world or the action with the highest Q-value
+        based on the value of the epsilon and the flip
+        """
+
+        # Flip coin with probability epsilon
+        flip = random.random()
+        # If the flip is less than epsilon the agent will "explore"
+        if flip < self.epsilon:
+            action = random.choice(self.getLegalActions(ryg_state))
+        # Otherwise, we the agent chooses the action with the highest Q-value
+        else:
+            action = self.computeActionFromQValues(ryg_state)
+        # Add action to log of actions taken
+        self.actions_taken.append(action)
+        return action
+
+    def update_qs(self, lane, action, afterStepLongestLane, reward):
+        """
+        Params:
+            lane (int) : ID of 1 of 8 lanes in the junction
+            action (str) : A legal traffic light state 
+            afterStepLongestLane (int) : ID of 1 of 8 lanes in the junction with the most number of vehicles
+            reward (float) : Reward calculated using the difference in current and previous wait time
+
+        Function to update the Q-values
+        """
+        # Add lane to Q-value dictionary if not yet initialized
+        if lane not in self.qs.keys():
+            self.qs[lane] = {}
+        # Q-value Update Rule
+        cur_q = self.getQValue(lane, action)
+        next_q = self.computeValueFromQValues(afterStepLongestLane, action)
+        self.qs[lane][action] = cur_q + self.alpha * \
+            (reward + self.gamma * next_q - cur_q)
+
+
+if __name__ == "__main__":
+    # Random Policy for the Traffic Signals
+    # Connect to script and intiate step counter
+    traci.start(["sumo", "-c", ANEESHA_PATH])
+    sim_rand = Sim(epsilon=1)
+    sim_rand.run_simulation()
+    # print(sim_rand.actions_taken)
+    # print(sim_rand.qs)
+    traci.close()
+
+    # Q-Learning for the Traffic Signals
+    # Connect to script and intiate step counter
+    traci.start(["sumo", "-c", ANEESHA_PATH])
+    sim_q = Sim()
+    sim_q.run_simulation()
+    # print(sim_q.actions_taken)
+    # print(sim_q.qs)
+    traci.close()
